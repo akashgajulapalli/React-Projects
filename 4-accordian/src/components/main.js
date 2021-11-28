d133d8822b16a66f02d0eec50c5c11abc2b8004e
Akash <gajulapalliakash23@gmail.om>
2021-11-28 13:47:56 +0530

    accirdion completed

---
+++ 4-accordian/src/components/main.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./question";
+
+const Main = () => {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main className="page">
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {questions.length > 0
+            ? questions.map((item) => {
+                return <SingleQuestion key={item.id} {...item} />;
+              })
+            : null}
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
